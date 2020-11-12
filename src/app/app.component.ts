cbbd5b9380701256f996d4387dc6c38a6f950830
rlexa <alexey.rempel@gmail.com>
2020-11-12 15:58:54 +0100

    prefix change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'dd-chords-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
