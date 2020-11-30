014f0e8c6d94221da70f25030170a6b997887d1e
rlexa <alexey.rempel@gmail.com>
2020-11-30 16:18:30 +0100

    wip small screen style

--- src/app/module/music/music.component.ts
+++ src/app/module/music/music.component.ts
@@ -1,4 +1,8 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, OnDestroy} from '@angular/core';
+import {DoneSubject, RxCleanup} from 'dd-rxjs';
+import {BehaviorSubject} from 'rxjs';
+import {filter, takeUntil} from 'rxjs/operators';
+import {DiTracksFilterPerformer} from '../di-music/di-tracks-filter-performer';
 
 @Component({
   selector: 'dd-chords-music',
@@ -6,4 +10,35 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
   styleUrls: ['./music.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MusicComponent {}
+export class MusicComponent implements OnDestroy {
+  constructor(
+    @Inject(DiTracksFilterPerformer) public readonly tracksFilterPerformer$: BehaviorSubject<string | null>,
+    private readonly changeDetectorRef: ChangeDetectorRef,
+  ) {
+    this.tracksFilterPerformer$
+      .pipe(
+        filter((performer) => !!performer),
+        takeUntil(this.done$),
+      )
+      .subscribe(() => {
+        this.visibleArea = 'tracks';
+        this.changeDetectorRef.markForCheck();
+      });
+  }
+
+  @RxCleanup() private readonly done$ = new DoneSubject();
+
+  visibleArea: 'track' | 'tracks' | 'performers' = 'performers';
+
+  destroy(): void {}
+  ngOnDestroy(): void {
+    this.destroy();
+  }
+
+  resetPerformer(): void {
+    if (this.visibleArea !== 'performers') {
+      this.visibleArea = 'performers';
+      this.tracksFilterPerformer$.next(null);
+    }
+  }
+}
