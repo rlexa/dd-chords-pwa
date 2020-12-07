4c04ab36a8e8cc237e2fd2c83955750366980dd8
rlexa <alexey.rempel@gmail.com>
2020-12-07 14:55:11 +0100

    add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {DiCommonModule} from './module/common/di-common';
@@ -8,7 +10,13 @@ import {RoutingService} from './module/common/routing/routing-service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, DiCommonModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    DiCommonModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
