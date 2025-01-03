a9f99c222a3e2c6614407dab62f892ebfed9c27f
Alex Rempel <alexey.rempel@googlemail.com>
2025-01-03 20:46:32 +0100

    fix tests

--- src/app/test.ts
+++
@@ -1,6 +0,0 @@
-import {Provider, Type} from '@angular/core';
-import {Mock} from 'ts-mockery';
-
-export const mockWith = <T>(provide: Type<T>, mock: T) => ({provide, useFactory: () => mock}) as Provider;
-
-export const mockAll = <T>(provide: Type<T>) => mockWith(provide, Mock.all<T>());
